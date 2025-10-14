09eb3004e29f2ac27d72de427ce898bd56700709
MD Miyam <mdmiyam20@gmail.com>
2025-10-14 13:13:39 +0600

    app exception added

---
+++ lib/data/app_exception.dart
@@ -0,0 +1,22 @@
+class AppException implements Exception {
+  final String? _prefix;
+  final String? _message;
+
+  AppException([this._prefix, this._message]);
+
+  String returnExceptionString() {
+    return '$_prefix$_message';
+  }
+}
+
+class InternetException extends AppException {
+  InternetException([String? message]) : super(message, 'No Internet');
+}
+
+class RequestTimeoutException extends AppException {
+  RequestTimeoutException([String? message]) : super(message, 'Request time out');
+}
+
+class ServerException extends AppException {
+  ServerException([String? message]) : super(message, 'Internal server error');
+}
